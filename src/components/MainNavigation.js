309664da762f40be87ab0264a9233ba638ab385e
afolabiopakunle <opakunleafolabi@gmail.com>
2022-05-26 20:50:50 +0100

    implemented navigation

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
-  return <header>
-      <div className={classes.header}>React Meetup</div>
+  return <header  className={classes.header}>
+      <div className={classes.logo}><Link to='/'>React Meetups</Link></div>
       <nav>
         <ul>
           <li><Link to='/'> All Meetups </Link></li>
-          <li><Link to='/new-meetup'> New Meetups </Link></li>
-          <li><Link to='/favourites'> My Favourite Meetups </Link></li>
+          <li><Link to='/new-meetup'> Add New Meetup</Link></li>
+          <li><Link to='/favourites'> My Favourite </Link></li>
         </ul>
       </nav>
     </header>
