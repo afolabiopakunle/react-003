9745bcb7a3b8542a88a4c56809d242be75ce8dfc
afolabiopakunle <opakunleafolabi@gmail.com>
2022-05-26 18:13:33 +0100

    basic return elements added

--- src/pages/AllMeetUps.js
+++ src/pages/AllMeetUps.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function AllMeetUps() {
-
+  return <div>All Meet Ups</div>
 }
 
 export default AllMeetUps;
--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function NewMeetUp() {
-
+  return <div>New Meet Up</div>
 }
 
 export default NewMeetUp;
